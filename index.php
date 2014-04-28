bcbf8328197913ef61d21486694086981862ff1f
Miguel Angel Torres Suarez <miguelangel.torres@outlook.com>
2014-04-28 23:42:06 +0200

    First commit.

---
+++ index.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Foundation | Welcome</title>
+        <link rel="stylesheet" href="css/foundation.css" />
+        <script src="js/vendor/modernizr.js"></script>
+    </head>
+    <body>
+        <div class="fixed">
+            <nav class="top-bar" data-topbar>
+                <ul class="title-area">
+                    <li class="name">
+                        <h1><a href="#">NetHyperLink :: Development Services</a></h1>
+                    </li>
+                    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
+                </ul>
+
+                <section class="top-bar-section">
+                    <!-- Right Nav Section -->
+                    <ul class="right">
+                        <li class="active"><a href="#">Right Button Active</a></li>
+                        <li class="has-dropdown">
+                            <a href="#">Right Button Dropdown</a>
+                            <ul class="dropdown">
+                                <li><a href="#">First link in dropdown</a></li>
+                                <li><a href="#">First link in dropdown</a></li>
+                                <li><a href="#">First link in dropdown</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <!-- Left Nav Section -->
+                    <ul class="left">
+                        <li><a href="#">Left Nav Button</a></li>
+                    </ul>
+                </section>
+            </nav>
+        </div>
+
+        <!-- End Header and Nav -->
+        <div class="row">
+            <div class="large-12 columns">
+                <hr />
+                <div class=" large-3 column">izquierdo</div>
+                <div class=" large-9 column">
+                    <div class="">
+                        <div class="button-bar">
+                            <ul class="button-group">
+                                <li><a href="#" class="small button [secondary success alert]">Home</a></li>
+                                <li><a href="#" class="small button [secondary success alert]">Button 3</a></li>
+                                <li><a href="#" class="small button [secondary success alert]">Button 4</a></li>
+                                <li><a href="#" class="small button [secondary success alert]">Button 5</a></li>
+                                <li><a href="#" class="small button [secondary success alert]">Button 6</a></li>
+                                <li><a href="#" class="small button [secondary success alert]">Button 7</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- First Band (Slider) -->
+        <div class="row">
+            <div class="large-12 columns">
+                <div id="slider">
+                    <img src="http://placehold.it/1000x400&text=[ img 1 ]" />
+                </div>
+
+                <hr />
+            </div>
+        </div>
+
+        <!-- Three-up Content Blocks -->
+
+        <div class="row">
+            <div class="large-4 columns">
+                <img src="http://placehold.it/400x300&text=[img]" />
+                <h4>This is a content section.</h4>
+                <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+            </div>
+
+            <div class="large-4 columns">
+                <img src="http://placehold.it/400x300&text=[img]" />
+                <h4>This is a content section.</h4>
+                <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+            </div>
+
+            <div class="large-4 columns">
+                <img src="http://placehold.it/400x300&text=[img]" />
+                <h4>This is a content section.</h4>
+                <p>Bacon ipsum dolor sit amet nulla ham qui sint exercitation eiusmod commodo, chuck duis velit. Aute in reprehenderit, dolore aliqua non est magna in labore pig pork biltong. Eiusmod swine spare ribs reprehenderit culpa. Boudin aliqua adipisicing rump corned beef.</p>
+            </div>
+
+        </div>
+
+        <!-- Call to Action Panel -->
+        <div class="row">
+            <div class="large-12 columns">
+
+                <div class="panel">
+                    <h4>Get in touch!</h4>
+
+                    <div class="row">
+                        <div class="large-9 columns">
+                            <p>We'd love to hear from you, you attractive person you.</p>
+                        </div>
+                        <div class="large-3 columns">
+                            <a href="#" class="radius button right">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <!-- Footer -->
+
+        <footer class="row">
+            <div class="large-12 columns">
+                <hr />
+                <div class="row">
+                    <div class="large-6 columns">
+                        <p>© Copyright no one at all. Go to town.</p>
+                    </div>
+                    <div class="large-6 columns">
+                        <ul class="inline-list right">
+                            <li><a href="#">Link 1</a></li>
+                            <li><a href="#">Link 2</a></li>
+                            <li><a href="#">Link 3</a></li>
+                            <li><a href="#">Link 4</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div> 
+        </footer>
+        <script src="js/vendor/jquery.js"></script>
+        <script src="js/vendor/fastclick.js"></script>
+        <script src="js/foundation.min.js"></script>
+        <script>
+            $(document).foundation();
+        </script>
+    </body>
+</html>
